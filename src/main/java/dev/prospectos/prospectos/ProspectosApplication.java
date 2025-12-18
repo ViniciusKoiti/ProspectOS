003c28d170978fab87a6cbdd878e30e7ba9f5712
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-12-18 20:45:32 -0300

    first commit

---
+++ src/main/java/dev/prospectos/prospectos/ProspectosApplication.java
@@ -0,0 +1,13 @@
+package dev.prospectos.prospectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProspectosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/prospectos/prospectos/ProspectosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prospectos.prospectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
